c24cda79680104da1bd453bd98be92df5c05872c
cgliss <43994946+cgliss@users.noreply.github.com>
2018-10-19 16:45:55 -0400

    Update RUN_all_Analysis.m

--- WholeTissuePipeline/RUN_all_Analysis.m
+++ WholeTissuePipeline/RUN_all_Analysis.m
@@ -21,15 +21,15 @@
 
 % 1) THE MASTER RAW DATA FOLDER
 
-master_folderIN = 'D:\Myeloma_HSF1_2018-07-27\RawData';
+master_folderIN = 'Y:\IN Cell Analyzer 6000\Carmen\Test';
 
 % 2) SLIDE AND PLATE SPECIFIC PARAMETERS: Comment out the other section
 
 % 2A) WHOLE TISSUE SLIDE: THE RAW DATA SUBFOLDERS FOR EACH SLIDES AND HOW MANY TILES THEY HAVE
 
-slides_folders = {'Myeloma_BS16_14824'};   % names of the folders for each slides
-slides_rowtils = [1,2];                         % number of rows in each slide
-slides_coltils = [2,10];                        % number of cols in each slide
+slides_folders = {'A_5B_dHSF2','A-1C_WT','B_5B_dHSF2', 'B-1C_WT'};   % names of the folders for each slides
+slides_rowtils = [5,5,5,5];                         % number of rows in each slide
+slides_coltils = [2,2,2,2];                        % number of cols in each slide
 maxfields = slides_rowtils.*slides_coltils;       % number of fields imaged
 %DO NOT CHANGE
     prefix1 = {'A - 1(fld '};    % prefix for the name of each file
@@ -59,21 +59,21 @@ maxfields = slides_rowtils.*slides_coltils;       % number of fields imaged
 
 % 3) THE RAW DATA SUBFOLDER FOR EACH CYCLE WITHIN EACH SLIDE
 
-cycles_folders = {'Cycle_1','Cycle_2','Cycle_3','Cycle_4'};
+cycles_folders = {'Cycle_1','Cycle_2','Cycle_3'};
 
 % 4) THE DATA FOLDER FOR BASIC CORRECTION IMAGES
 %Use imagej_basic_ashlar.py to create Basic Correction Images
-basic_folderloc = 'D:\Myeloma_HSF1_2018-07-27\Analysis\AnalysisWBaSiCcorrection\BasicCorrectionImages\Myeloma_14824\Cycle_';
+basic_folderloc = 'Y:\IN Cell Analyzer 6000\Carmen\Test\BasicCorrection\Cycle_';
 
 % 5) MONTAGES FOR ROI: If you do not have montages then leave both
 % variables as empty
-montfolder = 'X:\Carmen\Myeloma_Montages\ROIs_Montages\';   % base folder for montages
-montfols = {'ROI_1', 'ROI_2', 'ROI_3'};                     % names of the ROI for each montage 
+montfolder = '';   % base folder for montages
+montfols = {''};                     % names of the ROI for each montage 
 
 
 % 6) USER INPUT PARAMTERS
-cycles = [2 3 4 5]; %Cycle number is based on number in cycles_folders 
-dates = '\2018-07-27_'; 
+cycles = [1 2 3]; %Cycle number is based on number in cycles_folders 
+dates = '\2018-10-19'; 
 % marker_name = {'HSF1','Hsp70','Hsp90','CD138','CytoC'}; %List of markers
 % antibody_type = {'N', 'C', 'C','C'}; %Type of marker for each antibody; N= Nucleus; C= Cytoplasm 
 % antibody_rounds = [3, 6, 14, 7, 15]; %Numbering is 1=DAPI cycle 1; 2= 488 cycle 1; 3=555 cycle 1; 4=647 cycle 1 
@@ -81,7 +81,7 @@ dates = '\2018-07-27_';
 % canceround = 10; %Used to filter the data based on the cancer marker
 
 % THRESHOLD VALUES
-cellsize = 23.5;     %Threshold for cell segmentation 
+cellsize = 31;     %Threshold for cell segmentation (must be odd) 
 % sol_thresh = 0.9;    %Threshold to filter data based on cancer marker 
 
 
@@ -91,8 +91,8 @@ z_wave = 'wv Green - dsRed';  % Wavelength of the z-stack, possible wavelengths:
 z_num = 0;                      % Number of z-stacks 
 
 % 8) IF YOU NEED FOCI SEGMENTATION 
-HSF1cycle = 0; %Cycle is from 1... 
-HSF1round = 0; %Numbering is 1=DAPI cycle 1; 2= 488 cycle 1; 3=555 cycle 1; 4=647 cycle 1 
+HSF1cycle = 1; %Cycle is from 1... 
+HSF1round = 3; %Numbering is 1=DAPI cycle 1; 2= 488 cycle 1; 3=555 cycle 1; 4=647 cycle 1 
 thr = 2; %Foci threshold 
 
 %% Running analysis 
@@ -136,18 +136,18 @@ end
 
 %%
 disp('Run 1: Making Stacks')
-RUN_1_makecorestacks(master_folderIN, basefolder,basicfolderloc, slides_folders, maxfields, cycles, tilesize, prefix1, wavelengths, z_wave, z_cycle, max_rows, well_nums , well_lets, DAPIwv)
+%RUN_1_makecorestacks(master_folderIN, basefolder,basicfolderloc, slides_folders, maxfields, cycles, tilesize, prefix1, wavelengths, z_wave, z_cycle, max_rows, well_nums , well_lets, DAPIwv)
 disp('Run 2: Segmenting Cells')
 RUN_2_segmentstacks(basefolder, slides_folders, maxfields,DAPIslice, cellsize, max_rows, well_nums, well_lets)
 disp('Run 3: Foci Segmentation & making Measurements')
-RUN_3_CycIF_measurements(basefolder,slides_folders, maxfields, maxcycle, dates, DAPIslice, z_cycle, znum, thr, CYCLEslice, prefix1, max_rows, well_nums)  
+RUN_3_CycIF_measurements(basefolder,slides_folders, maxfields, maxcycle, dates, DAPIslice, z_cycle, z_num, thr, CYCLEslice, prefix1,max_rows, well_nums, well_lets, cycles)
 if ~isempty(montfols)
     disp('Removing ROIs')
     remove_ROI(slides_folders, slides_rowtils, slides_coltils, maxfields,montfolder, montfols) 
 end
 
-% disp('Run 4: Creating Graphs')
-RUN_4_CellStateCompare_Path(basefolder, slides_folders, dates,marker_name, antibody_rounds, marker_cycle, HSF1round, canceround, antibody_type, max_rows, well_nums)
+% % disp('Run 4: Creating Graphs')
+% RUN_4_CellStateCompare_Path(basefolder, slides_folders, dates,marker_name, antibody_rounds, marker_cycle, HSF1round, canceround, antibody_type, max_rows, well_nums)
 
 %% OVERVIEW AND EXPLANATION
 
@@ -155,7 +155,7 @@ RUN_4_CellStateCompare_Path(basefolder, slides_folders, dates,marker_name, antib
 % 1) RUN_1_makecorestacks
 % 2) RUN_2_segmentstacks
 % 3) RUN_3_CycIF_measurements
-% 4) CycIF_Segmentation_normfit_opencellsizeover5_medianpercell_v1
+% 4) CycIF_Segmentation_normfit_opencellsizeover5_medianpercell_v2
 % 5) findmininvect
 % 6) saveastiff
 
