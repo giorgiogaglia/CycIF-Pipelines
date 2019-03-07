d6b0b4d9cb19f92f296ce002b144822bb08a1ba2
cgliss <43994946+cgliss@users.noreply.github.com>
2019-03-07 15:10:19 -0500

    Add files via upload

--- WholeTissueAshlaredPipeline/RUN_ALL.m
+++ WholeTissueAshlaredPipeline/RUN_ALL.m
@@ -14,6 +14,8 @@ clear all
 % probabilities produce by Ilastik
 % Step 4: (RUN_Step4_CycIF_measurments_ilastik.m) Makes measurements of
 % signal and foci segmentation 
+% Step 5: (RUN_Step5_ROI.m) Finds pixel location of ROIs and indexes of
+% centroids within the ROI 
 % 
 % In order to begin the analysis a set of parameters are needed to be
 % defined by the user 
@@ -22,6 +24,13 @@ clear all
 % field 
 
 %%% OUTPUTS FILES AND LOCATIONS 
+% Step 1:
+% FullStacks: AllTheRawData\OmeTifName\FullStacks\OmeTifName_Field_row_column.tif
+% CroppedStacks: AllTheRawData\OmeTifName\CroppedData\OmeTifName_Field_row_column_#ofCrop.tif
+% Coordinates: AllTheRawData\Coordinates_FullStacks\OmeTifName.mat 
+% y= # of pixels in 1 column; x = # of pixels in 1 row ; t = size of field desired 
+% Coordinates Matrix: Coordinates.Field(row,column) = [keep field (0= no, 1=yes), x1, x2, y1, y2]
+% Montage: AllTheRawData\MontageforROI\OmeTifName_montage.tif 
 % Step 3:
 % Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Ilastik_Segmentation\OmeTifName_Field_row_column_Seg.tif
 % Check Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Ilastik_Segmentation\OmeTifName_Field_row_column_checkseg.tif
@@ -30,6 +39,9 @@ clear all
 % Foci Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Foci_Segmentation\OmeTifName_Field_row_column__HSF1_FociSeg.tif
 % Foci Check Segmented Images: AllTheRawData\ANALYSIS\OmeTifName\Foci_Segmentation\OmeTifName_Field_row_column__HSF1_FociSeg_check.tif
 % Measurments file: AllTheRawData\ANALYSIS\Analysis_Results\Results_data.mat 
+% Step 5:
+% ROI_pixels: AllTheRawData\ROIpixels\OmeTifName\ROI_pixels.mat: matrix of pixel locations (x,y) of ROIs, vector of row indexes of Centroids that are within the ROI  
+
 
 %%% INPUT FILE LOCATION AND FORMATTING
 %
@@ -44,6 +56,11 @@ clear all
 % eg. AllTheRawData\ANALYSIS\OmeTifName\FullStacks\Ilastik_Probabilities\SlideName_Field_row_column_Probabilities.tif 
 % eg. AllTheRawData\ANALYSIS\OmeTifName\FullStacks\SlideName_Field_row_column.tif 
 
+% ROI:
+% Must be drawn on ImageJ on the the montage created in RUN_Step1_new_fields_preilastik_crops
+% Location: Montage: AllTheRawData\MontageforROI\OmeTifName_montage.tif 
+% ROIs must be save in "AllTheRawData\MontageforROI\" and named with "OmeTifName" 
+
 % 1) THE MASTER OME.TIF DATA FOLDER
 
 filename.folders.main = 'W:\Analysis\Ashlared\';
@@ -51,13 +68,11 @@ filename.folders.main = 'W:\Analysis\Ashlared\';
 % 2) SLIDE SPECIFIC PARAMETERS:
 filename.folders.fols = {'AJ0160_P2', 'AJ0160_P3'}; %Name of ASHLARED image without '.ome.tif' ending 
 filename.cycles = 8; % total # of cycles
-filename.prefix1 = 10; % number larger then the max number of rows of fields
-filename.prefix2 = 10; % number larger then the max number of columns of fields 
 
 % 3) USER DESIRED PARAMETERS 
 filename.sizefield = 6000; %Size of field desired for a square 
 filename.crops = 2; %# of cropped fields desired per field 
-filename.dim = ['%02d']; %Delimeter 
+
 
 % 4) OPTIONS  
 % Step 3: SEGMENTATION OPTIONS 
@@ -82,10 +97,18 @@ filename.folders.ilastikseg = 'Ilastik_Segmentation\';
 filename.ilastiksuffix = '_Probabilities.tif';
 filename.folders.results = 'Analysis_Results\';
 filename.folders.fociseg = 'Foci_Segmentation\'; 
+filename.folders.ROI = 'ROIpixels\'; 
 filename.suffix = '.tif';
+filename.folders.montage = 'MontageforROI\'; 
+filename.dim = ['%02d']; %Delimeter 
 
-%% Runs Step 1: 
+%% MAKE SURE TO COMMENT OUT THE OTHER STEPS THAT YOU AREN'T AT YET!!
+% Runs Step 1: (Creating FullStacks, CroppedStacks, Montages) 
 RUN_Step1_new_fields_preilastik_crops(filename,options) 
+
 %% Runs Step 3 and 4: (Segmentation and Measurments) 
 RUN_Step3_segmentfromilastik(filename, options) 
 RUN_Step4_CycIF_measurements_ilastik(filename, options)  
+
+%% Run Step 5: (ROI Selection)
+RUN_Step5_ROI(filename, options)
